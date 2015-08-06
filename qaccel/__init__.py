4f3ecaf542f8d87f5ca80e4442814a288a3e9d8e
Matthew Harrigan <harrigan@stanford.edu>
2015-08-05 18:50:53 -0700

    format

--- qaccel/__init__.py
+++ qaccel/__init__.py
@@ -1,9 +1,10 @@
-
 def init_logging(level):
     import logging
 
     logging.basicConfig(level=level, handlers=[], style='{')
 
+
 from ._version import get_versions
+
 __version__ = get_versions()['version']
 del get_versions
