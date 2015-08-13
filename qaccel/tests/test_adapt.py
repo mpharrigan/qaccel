8303144bf7a941553733c1dd4c091b2e2f327be5
Matthew Harrigan <harrigan@stanford.edu>
2015-08-12 17:07:16 -0700

    partial test

--- qaccel/tests/test_adapt.py
+++ qaccel/tests/test_adapt.py
@@ -1,20 +1,25 @@
 from unittest import TestCase
-from qaccel.adapt import Random
+from qaccel.adapt import Random, TruePopWeighted
 import numpy as np
 
 
-class TestRandom(TestCase):
+class TestAdapt(TestCase):
     def setUp(self):
-        self.adapt = Random()
         self.params = {
             'n_states': 2,
             'tpr': 10,
         }
 
-    def test_adapt(self):
-        states = self.adapt.adapt(None, self.params)
+    def test_random(self):
+        adapt = Random()
+        states = adapt.adapt(None, self.params)
         self.assertEqual(len(states), 10)
         for s in states:
             self.assertLess(s, 2)
 
         self.assertLess(np.abs(np.mean(states) - 0.5), 0.4)
+
+    def test_truepopweight(self):
+        # TODO
+        pass
+
