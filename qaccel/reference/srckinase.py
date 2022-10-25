73cc0e9c5d27cd941264e73b5d8250a5037ada2a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-25 02:41:47 +0000

    Adding tarfile member sanitization to extractall()

--- qaccel/reference/srckinase.py
+++ qaccel/reference/srckinase.py
@@ -44,7 +44,26 @@ def _download(source, tar_dest, untar_dest):
         with open(tar_dest, 'wb') as tmat_tar_f:
             tmat_tar_f.write(tmat_tar_url.read())
         with tarfile.open(tar_dest) as tmat_tar_f:
-            tmat_tar_f.extractall(untar_dest)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tmat_tar_f, untar_dest)
 
 
 def _build_from_download(power, fmt):
